bc30f1b2ddb34b4e612e9a9a63ffc54ad912bb32
Swapnil Gandhale <swapnilgandhale08@gmail.com>
2025-07-11 15:32:17 +0530

    merging 3rd assignment

---
+++ JS/Ass3.js
@@ -0,0 +1,11 @@
+// Loops
+//for loop
+
+//calculate the sum of first n numbers
+ let sum = 0;
+ let n = prompt("Enter NO for N ");
+
+ for(let i = 1; i <= n; i++){
+   sum += i;
+ } 
+  alert("Sum Of Your Entered No is " + sum);
