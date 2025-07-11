e5406f063799d2a06e3615797841dc67ac01a2da
SWAPY8 <swapnilofficial08@gmail.com>
2025-07-11 15:50:56 +0530

    files are merged

--- JS/Ass3.js
+++ JS/Ass3.js
@@ -2,10 +2,34 @@
 //for loop
 
 //calculate the sum of first n numbers
- let sum = 0;
- let n = prompt("Enter NO for N ");
+//  let sum = 0;
+//  let n = prompt("Enter NO for N ");
 
- for(let i = 1; i <= n; i++){
-   sum += i;
- } 
-  alert("Sum Of Your Entered No is " + sum);
+//  for(let i = 1; i <= n; i++){
+//    sum += i;
+//  } 
+//   alert("Sum Of Your Entered No is " + sum);
+// find even no betn 1 to 100
+
+
+// for(let i = 0; i <= 100; i++){
+    
+//        console.log(i); 
+    
+// }
+
+// // let num = 100;
+// for(let i = 0; i <= 100; i++){
+//     if(i % 2 === 0){
+//        console.log(i); 
+//     }
+// }
+
+//random number guessing game
+
+RandomNumber = 
+num = alert(prompt("Guess The Number to Win 1Cr"));
+
+if(num = RandomNumber){
+    alert("You won the 1Cr");
+}
