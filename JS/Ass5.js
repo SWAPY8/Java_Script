eb0b40256e38f7e0bda6ed8457c88797c0d1d5bd
SWAPY8 <swapnilofficial08@gmail.com>
2025-07-13 19:45:02 +0530

    half done with function

---
+++ JS/Ass5.js
@@ -0,0 +1,49 @@
+// //Functions definition and calling
+// //sum function
+// let sum;
+// function sumfun(a, b, c){
+// sum = c + a + b;
+// console.log(sum);
+// return sum;
+// }
+
+// // console.log()
+// sumfun(6, 7, 5);
+// // console.log(sum);
+// sumfun(64, 57, 75);
+
+//Arrow function
+
+// const mult = (x, y, z) => {
+
+//     return  (x * y) / z;
+
+// }
+// mult(8, 554, 6);
+
+//calculate vowels in the string using function
+
+// function countVowels(str) {
+//   let count = 0;
+//   for (let val of str) {
+//     if (
+//       val === "a" ||
+//       val === "e" ||
+//       val === "i" ||
+//       val === "o" ||
+//       val === "u"
+//     ) {
+//       count++;
+//     }
+//   }
+//   console.log("count: ", count)
+
+// }
+// countVowels("Striiiiing");
+
+// //arrow function
+
+// let count
+// const countfunction = (srt) => {
+//     for(let i = 0; )
+// }
