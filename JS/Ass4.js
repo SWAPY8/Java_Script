0cf12ea65dd3b6cf52e0f94231b3adb2c4b63cdc
SWAPY8 <swapnilofficial08@gmail.com>
2025-07-13 18:32:05 +0530

    done with array methods

--- JS/Ass4.js
+++ JS/Ass4.js
@@ -24,16 +24,40 @@
 //   alert(`Average marks of class is ${total/len}`)
 
 
-//apply offer of 10% to every price
+//apply offer of 10% to every price using for off, for loop
 
-let prices = [799, 77, 200, 900, 5555];
-let offer;
-let i = 0;
-for(let el of prices){
-  offer = el / 10; 
-  prices[i]-=offer;
-  i++;
-  console.log(prices);
-}
+//  let prices = [799, 77, 200, 900, 5555];
+// let offer;
+// let i = 0;
+// for(let el of prices){
+//   offer = el / 10; 
+//   prices[i]-=offer;
+//   i++;
+//   // console.log(prices);
+// }
 
-// console.log(finalprice);
+// console.log(prices);
+
+// for loop
+
+// for(let i = 0; i < prices.length; i++){
+//   let offer = prices[i]/10;
+//   prices[i] -= offer;
+
+// }
+// console.log(prices);
+
+//array methods
+
+let comp = ["Google", "Microsoft", "Wipro", "Uber", "Blueberge"];
+
+console.log(comp);
+
+comp.shift();
+
+comp.splice(2, 1,  "Ola");
+
+comp.push("Tata");
+
+console.log(comp);
+
