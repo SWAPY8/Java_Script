20fc5c2de3bff9c462b3b3586b78ed80791b9c53
Swapnil Gandhale <swapnilgandhale08@gmail.com>
2025-07-14 16:51:10 +0530

    DOM Mnipulation part2

--- JS/Ass6.js
+++ JS/Ass6.js
@@ -1,14 +1,37 @@
-let old = document.querySelector("h1");
-let newtext = document.createTextNode(" Lets Celebrate...");
+// let old = document.querySelector("h1");
+// let newtext = document.createTextNode(" Lets Celebrate...");
 
-old.appendChild(newtext);
+// old.appendChild(newtext);
 
 //accessing div
 
 
 
-let boxes = document.querySelectorAll(".box");
+// let boxes = document.querySelectorAll(".box");
 
-boxes[0].innerText = "Welcome To World of HTML";
-boxes[1].innerText = "Welcome To World of CSS"
-boxes[2].innerText = "Welcome To World of JS";
+// boxes[0].innerText = "Welcome To World of HTML";
+// boxes[1].innerText = "Welcome To World of CSS"
+// boxes[2].innerText = "Welcome To World of JS";
+
+
+//create new btn and append with some styling at the end
+
+// let newbtn = document.createElement("button");
+// newbtn.innerText = " Submit ";
+// newbtn.style.color = "white";
+// newbtn.style.backgroundColor = "green";
+
+// let parent = document.querySelector("body");
+
+
+
+// parent.insertBefore(
+//     newbtn, parent.firstChild
+// );
+
+
+let newclass = document.querySelector(".first");
+// newclass.setAttribute("class", "second")
+
+newclass.classList.add("second")
+
