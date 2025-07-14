d6ba3a4d7a70df789440660e19eb4c7b1bf67081
Swapnil Gandhale <swapnilgandhale08@gmail.com>
2025-07-14 13:09:33 +0530

    start with DOM Mnipulation

---
+++ JS/Ass6.js
@@ -0,0 +1,14 @@
+let old = document.querySelector("h1");
+let newtext = document.createTextNode(" Lets Celebrate...");
+
+old.appendChild(newtext);
+
+//accessing div
+
+
+
+let boxes = document.querySelectorAll(".box");
+
+boxes[0].innerText = "Welcome To World of HTML";
+boxes[1].innerText = "Welcome To World of CSS"
+boxes[2].innerText = "Welcome To World of JS";
