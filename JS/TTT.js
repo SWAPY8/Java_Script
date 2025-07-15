7e411b383f376f526d5f1ca7fdbb64fc970305c7
SWAPY8 <swapnilofficial08@gmail.com>
2025-07-15 12:46:10 +0530

    Tic-Tac-Toe

---
+++ JS/TTT.js
@@ -0,0 +1,47 @@
+let boxes = document.querySelectorAll(".box");
+let turnp1 = true;
+
+let winpatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turnp1 === true) {
+      box.innerText = "X";
+      turnp1 = false;
+    } else {
+      box.innerText = "O";
+      turnp1 = true;
+    }
+    box.disabled = true;
+    winner();
+  });
+});
+
+let winner = () => {
+    for(let pat of winpatterns){
+        let pos1val = boxes[pat[0]].innerText;
+        let pos2val = boxes[pat[1]].innerText;
+        let pos3val = boxes[pat[2]].innerText;
+     if(pos1val != "" && pos2val != "" &&pos3val != "" ){
+        if(pos1val === pos2val &&  pos2val === pos3val){
+            document.getElementsByClassName("winner").innerText = "winner " , pos1val  , " Winner Winnner, Chicken Dinner!!!!!!"
+           
+            reset();
+        }
+     }   
+    }
+
+}
+let playagain = document.getElementsByClassName("resetgame");
+let reset = () => {
+   
+}
